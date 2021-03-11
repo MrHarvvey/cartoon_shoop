b57769d01774776676c50606b5b569d2b66188d8
MrHarvvey <oskarflorczak@gmail.com>
2021-03-11 23:44:23 +0100

    shop repair item adding

--- cartoon_shoop/views.py
+++ cartoon_shoop/views.py
@@ -35,25 +35,18 @@ def updateItem(request):
 	data = json.loads(request.body)
 	productId = data['productId']
 	action = data['action']
-	print('action', action)
-	print('productid', productId)
 	customer = request.user.customer
 	product = Product.objects.get(id=productId)
 	order, created =Order.objects.get_or_create(customer=customer, complete=False)
-
 	orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
-	print(orderItem, created)
 	if action == 'add':
-		orderItem.quantity =+ 1 
+		orderItem.quantity += 1
+		print(orderItem.quantity)
+		orderItem.save()
 	elif action == 'remove':
 		orderItem.quantity =- 1 
-	orderItem.safe()
+		orderItem.save()
 
 	if orderItem.quantity <= 0:
 		orderItem.delete()
-		
-
-
-
-
 	return JsonResponse('Item was added', safe=False)
--- static/js/cart.js
+++ static/js/cart.js
@@ -37,6 +37,5 @@ function updateUserOrder(productId, action){
 	
 	.then((data) =>{
 		console.log('data:', data)
-		location.reload
 	})
 }
